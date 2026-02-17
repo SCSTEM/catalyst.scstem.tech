982a837a892a899cecc01a850ccbf66b7a9a9050
Carson Seese <carson@carsonseese.com>
2026-02-17 18:39:19 +0000

    Batch user queries

--- packages/worker/src/routes/users.ts
+++ packages/worker/src/routes/users.ts
@@ -17,27 +17,30 @@ export const usersRoute = new Hono<{ Bindings: Bindings }>().get(
     const userId = c.req.param("userId");
     const limit = c.req.valid("query")?.limit ?? 50;
 
-    const emojis = await db
-      .select({
-        emoji: userEmojiCounts.emoji,
-        count: userEmojiCounts.count,
-      })
-      .from(userEmojiCounts)
-      .where(
-        and(eq(userEmojiCounts.userId, userId), gt(userEmojiCounts.count, 0)),
-      )
-      .orderBy(desc(userEmojiCounts.count))
-      .limit(limit);
-
-    const user = await db
-      .select({
-        userId: users.userId,
-        displayName: users.displayName,
-        avatarUrl: users.avatarUrl,
-      })
-      .from(users)
-      .where(eq(users.userId, userId))
-      .get();
+    const [emojis, [user]] = (await db.batch([
+      db
+        .select({
+          emoji: userEmojiCounts.emoji,
+          count: userEmojiCounts.count,
+        })
+        .from(userEmojiCounts)
+        .where(
+          and(eq(userEmojiCounts.userId, userId), gt(userEmojiCounts.count, 0)),
+        )
+        .orderBy(desc(userEmojiCounts.count))
+        .limit(limit),
+      db
+        .select({
+          userId: users.userId,
+          displayName: users.displayName,
+          avatarUrl: users.avatarUrl,
+        })
+        .from(users)
+        .where(eq(users.userId, userId)),
+    ])) as [
+      { emoji: string; count: number }[],
+      { userId: string; displayName: string; avatarUrl: string }[],
+    ];
 
     return c.json({ user: user ?? null, emojis });
   },
