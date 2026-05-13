2cf0c126aff451cb0233a21999824d26a0c10c98
Carson Seese <carson@carsonseese.com>
2026-05-13 11:19:03 -0400

    Filter out parrots with 0 reactions

--- packages/web/src/routes/stats_.parrots.lazy.tsx
+++ packages/web/src/routes/stats_.parrots.lazy.tsx
@@ -17,7 +17,8 @@ function RouteComponent() {
       <div className="flex flex-col gap-6 mx-auto md:max-w-2xl p-4 pt-4 md:py-8">
         <GlowingText text="Parrots" className="text-parrot text-4xl mx-auto" />
         {parrots
-          ?.sort((a, b) => b.count - a.count)
+          ?.filter((p) => p.count > 0)
+          .sort((a, b) => b.count - a.count)
           .map((p) => (
             <ParrotProfileCard key={p.name} emoji={p.name} />
           ))}
